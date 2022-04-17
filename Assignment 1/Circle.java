e9057f23cf81db8e11dfbadc91aefa2792d73678
Nas16iara <86537691+Nas16iara@users.noreply.github.com>
2022-04-17 12:55:13 -0400

    Update Circle.java

--- Assignment 1/Circle.java
+++ Assignment 1/Circle.java
@@ -1,14 +1,19 @@
 package RanasiaC;
 
 public class Circle {
+    //Attributes:
     private double radius;
+    //Constructors:
     Circle(double rad){
         radius=rad;
     }
+    //Methods:
      double circumference(){
-        return (2 * Math.PI * radius);
+        double circumferenceCircle=(2 * Math.PI * radius);
+        return circumferenceCircle;
     }
     double area(){
-        return (Math.PI*Math.pow(radius,2));
+        double areaCircle=Math.PI*Math.pow(radius,2));
+        return areaCircle;
     }
 }
